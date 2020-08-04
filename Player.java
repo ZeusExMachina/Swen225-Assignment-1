33402801e3ba52981514f7969193ae4da90269e2
ZeusExMachina <50428456+ZeusExMachina@users.noreply.github.com>
2020-08-05 11:36:39 +1200

    Add files via upload

--- Player.java
+++ Player.java
@@ -1,5 +1,4 @@
-import java.util.Set;
-import java.util.HashSet;
+import java.util.*;
 
 public class Player {
 	private final Set<Card> hand;
@@ -15,4 +14,24 @@ public class Player {
 	public void clearHand() { hand.clear(); }
 	
 	public String toString() { return "name: " + name + ", in hand: " + hand.toString(); }
+	
+	public boolean suggest(Game game) {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Make a suggestion - type in 3 cards:");
+		Card firstCard = askForCard(game, scan);
+		Card secondCard = askForCard(game, scan);
+		Card thirdCard = askForCard(game, scan);
+		CardTuple suggestion = new CardTuple(firstCard, secondCard, thirdCard);
+		return game.refutationProcess(this, suggestion);
+	}
+	
+	public Card askForCard(Game game, Scanner scan) {
+		System.out.print("Card: ");
+		Card card = game.getCard(scan.nextLine());
+		while (card == null) {
+			System.out.println("Invalid card. Try again.\nCard:");
+			card = game.getCard(scan.nextLine());
+		}
+		return card;
+	}
 }
