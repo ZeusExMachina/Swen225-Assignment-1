34b7f29ad1980b6d87a1191f3dc803ef717297ae
Jared Boult <jaredboult@gmail.com>
2020-08-07 13:42:41 +1200

    Update Piece.java

--- Piece.java
+++ Piece.java
@@ -6,9 +6,16 @@ public class Piece {
 	public Piece(String icon, Location loc) {
 		this.displayIcon = icon;
 		this.location = loc;
+		this.location.occupied = true;
 	}
 	
 	public String icon() { return displayIcon; }
 	
 	public Location location() { return location; }
+
+	public void setLocation(Location loc){
+		this.location.occupied = false;
+		this.location = loc;
+		this.location.occupied = true;
+	}
 }
